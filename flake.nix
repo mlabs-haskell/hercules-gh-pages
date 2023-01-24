3e282663943f8b01edf22ab4fe1481c21d587c26
Andrey Vlasov <andrassy_anderson@protonmail.com>
2023-01-24 16:30:50 +0300

    Do not hardcode repo owner&name

--- flake.nix
+++ flake.nix
@@ -47,7 +47,7 @@
                     githubHostKey = "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==";
                   in
                   ''
-                    set -x
+                    echo ${primaryRepo.remoteHttpUrl}
                     echo ${githubHostKey} >> ~/.ssh/known_hosts
                     export GIT_COMMITTER_NAME="${committer.name}"
                     export GIT_COMMITTER_EMAIL="${committer.email}"
@@ -61,7 +61,7 @@
                       ''}
                     cp -r --no-preserve=mode ${gh-pages} ./gh-pages && cd gh-pages
                     git init -b ${branchName}
-                    git remote add origin https://mlabs-haskell:`readSecretString git .token`@github.com/mlabs-haskell/hercules-gh-pages.git
+                    git remote add origin https://${primaryRepo.owner}:`readSecretString git .token`@github.com/${primaryRepo.owner}/${primaryRepo.name}.git
                     git add .
                     git commit -m "Deploy to ${branchName}"
                     git push -f origin ${branchName}:${branchName}
