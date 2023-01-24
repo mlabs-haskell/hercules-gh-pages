5d1943f3650fcc5330238aceb1569d566ac03779
Andrey Vlasov <andrassy_anderson@protonmail.com>
2023-01-24 15:12:28 +0300

    Fix tokens

--- flake.nix
+++ flake.nix
@@ -22,21 +22,20 @@
           hci-effects.mkEffect {
             buildInputs = with pkgs; [ openssh git ];
             secretsMap = {
-              "ssh" = "clarity-ctl-types-github-deploy-ssh";
+              git = { type = "GitToken"; };
             };
             effectScript =
               let
                 githubHostKey = "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==";
               in
               ''
-                writeSSHKey
                 echo ${githubHostKey} >> ~/.ssh/known_hosts
                 export GIT_AUTHOR_NAME="Hercules-CI Effects"
                 export GIT_COMMITTER_NAME="Hercules-CI Effects"
                 export EMAIL="github@croughan.sh"
                 cp -r --no-preserve=mode ${self.packages.${system}.gh-pages} ./gh-pages && cd gh-pages
                 git init -b gh-pages
-                git remote add origin git@github.com:mlabs-haskell/hercules-gh-pages.git
+                git remote add origin https://mlabs-haskell:''${readSecretString git .token}/mlabs-haskell/hercules-gh-pages.git
                 git add .
                 git commit -m "Deploy to gh-pages"
                 git push -f origin gh-pages:gh-pages
