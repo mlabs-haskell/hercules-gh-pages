ee25b47b73dec0c65f5e5bd40f2c39c088966b2b
Andrey Vlasov <andrassy_anderson@protonmail.com>
2023-01-25 22:40:28 +0300

    Use aeson instead of readSecretString

--- effect.hs
+++ effect.hs
@@ -3,13 +3,12 @@
 module Main (main) where
 
 import Control.Monad (when)
-import Data.Aeson
+import Data.Aeson (FromJSON(parseJSON), eitherDecodeFileStrict', withObject, (.:))
 import Data.Text (Text)
 import qualified Data.Text as Text (unpack)
 import Text.URI
 import Turtle (MonadIO, fromText)
 import Turtle.Prelude
-import Turtle.Line (lineToText)
 import Turtle.Shell (FoldShell(FoldShell), foldShell, sh, liftIO)
 
 need' :: (MonadIO m, MonadFail m) => Text -> (Text -> m a) -> m a
@@ -23,13 +22,22 @@ fromRightM :: (MonadFail m, Show e) => Either e a -> m a
 fromRightM (Right ok) = pure ok
 fromRightM (Left e) = fail $ "fromRightM: " <> show e
 
+newtype Secrets = Secrets { gitToken :: Text }
+
+instance FromJSON Secrets where
+  parseJSON =
+    withObject "secrets" \secrets ->
+      secrets .: "git" >>= withObject "git" \git ->
+        git .: "data" >>= withObject "data" \data_ ->
+          data_ .: "token"
+
 main :: IO ()
 main = sh do
   secretsPath <- need' "HERCULES_CI_SECRETS_JSON" pure
   liftIO $ putStrLn =<< readFile (Text.unpack secretsPath)
-  -- () <- liftIO $ eitherDecodeFileStrict' (Text.unpack secretsPath) >>= \case
-  --   Left e -> fail $ show e
-  --   Right ok -> pure ok
+  Secrets {gitToken} <- liftIO $ eitherDecodeFileStrict' (Text.unpack secretsPath) >>= \case
+    Left e -> fail $ show e
+    Right ok -> pure ok
 
   branchName <- need' "branchName" pure
   ghPages <- need' "gh-pages" pure
@@ -37,9 +45,9 @@ main = sh do
   remoteHttpUrl <- need' "remoteHttpUrl" mkURI
   rewriteHistory <- need' "rewriteHistory" (pure . (== "1"))
 
-  token <- inproc "readSecretString" ["git", ".token"] mempty >>= mkPassword . lineToText
+  gitTokenPassword <- mkPassword gitToken
   authority <- fromRightM $ uriAuthority remoteHttpUrl
-  let origin = remoteHttpUrl { uriAuthority = Right authority { authUserInfo = Just UserInfo { uiUsername = owner, uiPassword = Just token } } }
+  let origin = remoteHttpUrl { uriAuthority = Right authority { authUserInfo = Just UserInfo { uiUsername = owner, uiPassword = Just gitTokenPassword } } }
   if rewriteHistory then do
     mkdir "gh-pages"
     cd "gh-pages"
