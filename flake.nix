a770a51e771c945c00641c10b97237aee76b75ef
Andrey Vlasov <andrassy_anderson@protonmail.com>
2023-01-25 13:50:15 +0300

    Remove GH SSH key
    Fix cp
    
    Do not commit when no changes

--- flake.nix
+++ flake.nix
@@ -42,7 +42,6 @@
                 # Env variables
                 inherit branchName;
                 inherit (primaryRepo) owner remoteHttpUrl;
-                githubHostKey = "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==";
                 ghPages = gh-pages;
                 GIT_COMMITTER_NAME = committer.name;
                 GIT_COMMITTER_EMAIL = committer.email;
@@ -55,7 +54,6 @@
                     set -e
                     TOKEN=`readSecretString git .token`
                     ORIGIN=`echo $remoteHttpUrl | sed "s#://#://$owner:$TOKEN@#"`
-                    echo githubHostKey >> ~/.ssh/known_hosts
                     if [[ $rewriteHistory -eq 1 ]]
                     then
                       mkdir ./gh-pages
@@ -66,10 +64,14 @@
                       git clone --branch $branchName --single-branch $ORIGIN gh-pages
                       cd gh-pages
                     fi
-                    cp -r --no-preserve=mode $ghPages .
-                    git add .
-                    git commit -m "Deploy to $branchName" --allow-empty
-                    git push -f origin $branchName:$branchName
+                    rm -r *
+                    cp -r --no-preserve=mode -T $ghPages .
+                    if [ -n "`git status --porcelain`" ]
+                    then
+                      git add .
+                      git commit -m "Deploy to $branchName"
+                      git push -f origin $branchName:$branchName
+                    fi
                   '';
               }
             );
